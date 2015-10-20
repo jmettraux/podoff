f30a157ee4e3fa1a55acb1d7dac68836b535fab6
John Mettraux <jmettraux@gmail.com>
2015-10-20 13:12:11 +0900

    scaffold spec/

---
+++ spec/core_spec.rb
@@ -0,0 +1,13 @@
+
+#
+# specifying podoff
+#
+# Tue Oct 20 13:11:38 JST 2015
+#
+
+require 'spec_helper'
+
+
+describe Podoff do
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+
+#
+# Specifying podoff
+#
+# Tue Oct 20 13:10:29 JST 2015
+#
+
+require 'pp'
+
+require 'podoff'
+
