7dff61e6c78a285b1019cb3d15cc2be7e5e39719
John Mettraux <jmettraux@gmail.com>
2015-10-22 07:04:44 +0900

    add more load specs

--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -14,6 +14,26 @@ describe Podoff do
 
     it 'loads a PDF document' do
 
+      d = Podoff.load('pdfs/t0.pdf')
+
+      expect(d.class).to eq(Podoff::Document)
+      expect(d.xref).to eq(412)
+      expect(d.objs.keys).to eq([ '1 0', '2 0', '3 0', '4 0', '5 0', '6 0' ])
+    end
+
+    it 'loads a PDF document' do
+
+      d = Podoff.load('pdfs/udocument0.pdf')
+
+      expect(d.class).to eq(Podoff::Document)
+      expect(d.xref).to eq(3138351)
+      expect(d.objs.size).to eq(273)
+      expect(d.objs.keys).to include('1 0')
+      expect(d.objs.keys).to include('273 0')
+    end
+
+    it 'loads a PDF document with incremental updates' do
+
       d = Podoff.load('pdfs/t1.pdf')
 
       expect(d.class).to eq(Podoff::Document)
