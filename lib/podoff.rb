bcaeaa7531fa573c095f892774f490cea4515239
John Mettraux <jmettraux@gmail.com>
2015-10-22 06:06:15 +0900

    get rid of Obj#prepend_text

--- lib/podoff.rb
+++ lib/podoff.rb
@@ -247,30 +247,6 @@ module Podoff
 
       x ? [ w - x, h - y ] : nil
     end
-
-    def prepend_text(x, y, text, opts={})
-
-      o = find { |o| o.index('BT') }
-      fail ArgumentError.new('found no BT in the tree') unless o
-
-      font = opts[:font] || o.font_names.first || 'TT0'
-      size = opts[:size] || 10
-      comm = opts[:comment]
-
-      i = o.index('BT')
-      bt = []
-      bt << 'BT'
-      bt << "#{x} #{y} Td"
-      bt << "/#{font} #{size} Tf"
-      bt << "(#{text})Tj"
-      bt << 'ET'
-      bt << " % #{comm}" if comm
-      bt = bt.join(' ')
-
-      o.lines.insert(i, bt)
-
-      o
-    end
   end
 end
 
--- spec/update_spec.rb
+++ spec/update_spec.rb
@@ -15,22 +15,22 @@ describe Podoff do
     @d = Podoff.load('pdfs/udocument0.pdf')
   end
 
-  describe 'Obj.prepend_text' do
-
-    it 'adds text at the beginning of an obj' do
-
-      d = @d.dup
-
-      p1 = d.page(1)
-      p1.prepend_text(0, 500, 'hello world! xxx', size: 35, font: 'Helv')
-      #p1.prepend_text(0, 450, '"stuff NADA"', size: 35, font: 'Helv')
-
-      d.write('tmp/out.pdf')
-
-      s = `grep -a "hello world!" tmp/out.pdf`.strip
-
-      expect(s).to eq('BT 0 500 Td /Helv 35 Tf (hello world! xxx)Tj ET')
-    end
-  end
+#  describe 'Obj.prepend_text' do
+#
+#    it 'adds text at the beginning of an obj' do
+#
+#      d = @d.dup
+#
+#      p1 = d.page(1)
+#      p1.prepend_text(0, 500, 'hello world! xxx', size: 35, font: 'Helv')
+#      #p1.prepend_text(0, 450, '"stuff NADA"', size: 35, font: 'Helv')
+#
+#      d.write('tmp/out.pdf')
+#
+#      s = `grep -a "hello world!" tmp/out.pdf`.strip
+#
+#      expect(s).to eq('BT 0 500 Td /Helv 35 Tf (hello world! xxx)Tj ET')
+#    end
+#  end
 end
 
