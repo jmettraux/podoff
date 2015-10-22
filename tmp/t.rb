ddc558d9908a3bf0040cf1b12240ff84569d4048
John Mettraux <jmettraux@gmail.com>
2015-10-23 06:12:59 +0900

    draft api for document.add* / obj.insert*

--- tmp/t.rb
+++ tmp/t.rb
@@ -5,6 +5,17 @@ d = Podoff.load('tmp/uig.pdf')
 
 p1 = d.page(1)
 
+  # additions
+  #
+#o = document.add_base_font(x)
+#o = document.add_stream(x)
+#o = document.re_add(o0) # replicate
+
+  # object tweaking
+  #
+#obj.insert_content(o / o.ref)
+#obj.insert_font(nick, o / o.ref)
+
 fo = d.add_base_font('Helvetica')
 
 st =
@@ -18,6 +29,14 @@ st =
       (Stuff NADA) Tj
     ET
   }.strip)
+
+  # idea
+  #
+#st = d.add_stream {
+#  tf '/Helvetica', 35
+#  bt 100, 650, 'Hello World Again!'
+#  bt 300, 750, 'Stuff NADA',
+#}
 
 p1 = p1.insert_content(st)
 p1.add_to_fonts('Helvetica', fo.ref)
