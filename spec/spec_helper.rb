d9348b76ff64b992d6133b16b65bb93e958d3b67
John Mettraux <jmettraux@gmail.com>
2015-11-11 14:06:20 +0900

    ask Vim for the fileencoding

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,9 +25,23 @@ RSpec::Matchers.define :be_a_valid_pdf do
 
     file_cmd =
       /darwin/.match(RUBY_PLATFORM) ? 'file -I' : 'file -i'
+    vim_cmd =
+      "vim -c 'execute \"silent !echo \" . &fileencoding | q'"
 
-    $qpdf_r = `#{file_cmd} #{path}; echo; qpdf --check #{path} 2>&1`
+    cmd = [
+      "echo '* vim :'",
+      "#{vim_cmd} #{path}",
+      "echo '* #{file_cmd} :'",
+      "#{file_cmd} #{path}",
+      "echo",
+      "qpdf --check #{path}"
+    ]
+    $qpdf_r = `(#{cmd.join('; ')}) 2>&1`
+      `#{file_cmd} #{path}; echo; qpdf --check #{path} 2>&1`
+
     $qpdf_r = "#{$qpdf_r}\nexit: #{$?.exitstatus}"
+#puts "." * 80
+#puts $qpdf_r
 
     $qpdf_r.match(/exit: 0$/)
   end
